26c11814370a41a875a5c945a1a1d1eff4a1a3f5
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-09-24 19:38:30 -0300

    Criando imageField e configura MEDIA_URL

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+# id (primary key -- automatico)
+
+class Contact(models.Model):
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  phone = models.CharField(max_length=50)
+  email = models.EmailField(max_length=254)
+  created_date = models.DateTimeField(default=timezone.now)
+  description = models.TextField(blank=True)
+  show = models.BooleanField(default=True)
+  picture = models.ImageField(blank=True, upload_to= 'pictures/%Y/%m/')
+
+
+  def __str__(self) -> str:
+    return f'{self.first_name} {self.last_name}'
